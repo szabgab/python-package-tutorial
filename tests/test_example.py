73b6d99242541594bd40029da1d5116eba0e2a86
Gabor Szabo <gabor@szabgab.com>
2022-10-07 12:19:17 +0300

    add import path to the test file

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'src'))
+
 from example_package_szabgab.example import add_one
 
 def test_one():
-    print('add_one to 1')
-    assert add_one(1) == 2
+    print('add_one to 2 is 3')
+    assert add_one(2) == 3
 
